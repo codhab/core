26c2b197138c2af42485e457297909d89addcf35
Elton Silva <elton.chrls@gmail.com>
2017-03-25 15:56:53 -0300

    Add recadastre

--- app/policies/core/attendance/ticket_policy.rb
+++ app/policies/core/attendance/ticket_policy.rb
@@ -2,11 +2,42 @@ module Core
   module Attendance
     class TicketPolicy < ApplicationPolicy
 
+      def allow_close?
+        case self.context_id 
+        when 1
+          !self.actions.where(situation_id: [1,2]).present?
+        when 2
+          !self.actions.where(situation_id: [1,2]).present?
+        when 3
+          !self.actions.where(situation_id: [1,2]).present? &&
+          self.actions.where(situation_id: [3,4]).present?
+        when 4
+          !self.actions.where(situation_id: [1,2]).present? &&
+          self.actions.where(situation_id: [3,4]).present?
+        when 5
+          !self.actions.where(situation_id: [1,2]).present? &&
+          self.actions.where(situation_id: [3,4]).present?
+        end
+
+      end
+
       def confirmation_required? action
         self.context.confirmation_required &&
         action.situation_id == 1
       end
 
+      def open? action 
+        [1,2].include?(action.situation_id) 
+      end
+
+      def closed? action 
+        !open?(action)
+      end
+
+      def input_disabled? action
+        return true if closed?(action)
+        (action.situation_id == 1 && self.context.confirmation_required)
+      end
     end
   end
 end
