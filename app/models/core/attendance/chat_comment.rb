7d8b0fd5b29383a8dfd3db17991a23d291fca277
igormarjes <igormarjes@gmail.com>
2017-03-27 14:43:24 -0300

    Update model

--- app/models/core/attendance/chat_comment.rb
+++ app/models/core/attendance/chat_comment.rb
@@ -7,8 +7,10 @@ module Core
 
       belongs_to :chat,  required: false, class_name: ::Core::Attendance::Chat
       belongs_to :staff, required: false, class_name: ::Core::Person::Staff
-      
+
       validates :content, presence: true
+
+      has_many :chat_uploads
 
     end
   end
