e8ae927fb1a369716a81d04443c85dafa411a3cd
igormarjes <igormarjes@gmail.com>
2017-03-31 19:33:45 -0300

    ok

--- app/presenters/core/attendance/chat_presenter.rb
+++ app/presenters/core/attendance/chat_presenter.rb
@@ -3,11 +3,11 @@ require_dependency 'core/application_presenter'
 module Core
   module Attendance
     class ChatPresenter < ApplicationPresenter
-      
+
       def protocol
-        "#{self.id}#{self.created_at.strftime('%Y')}"
+        "#{self.id}/#{self.created_at.strftime('%Y')}"
       end
-      
+
     end
   end
 end
