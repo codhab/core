8ce1d0f8d614e228e474b2383064074533f3d6e8
Elton Silva <elton.chrls@gmail.com>
2017-04-02 17:42:02 -0300

    ok

--- app/forms/core/attendance/avatar_form.rb
+++ app/forms/core/attendance/avatar_form.rb
@@ -2,7 +2,7 @@ module Core
   module Attendance
     class AvatarForm < Core::Candidate::Cadastre 
      
-      validates :avatar, file_size: { less_than_or_equal_to: 10.megabytes },
+      validates :avatar, file_size: { less_than_or_equal_to: 50.megabytes },
                          file_content_type: { allow: ['image/jpeg', 'image/png'] },
                          allow_blank: true 
      
