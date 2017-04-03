00c350ea7214403757e0abbcbe84be3996474ee6
igormarjes <igormarjes@gmail.com>
2017-04-02 22:11:20 -0300

    Update presenters

--- app/presenters/core/candidate/cadastre_presenter.rb
+++ app/presenters/core/candidate/cadastre_presenter.rb
@@ -96,17 +96,19 @@ module Core
      end
 
      def zone?
-         case self.income
-         when 0..1600
-           [1, 0, 1600]
-         when 1601..3275
-           [2, 1601, 3275]
-         when 3276..5000
-           [3, 3276, 5000]
-         else
-           [4, 5001, 99999]
-         end
-     end
+        case self.income
+        when 0..1800
+          [1, 0, 1800]
+        when 1800.01..2350
+          [1.5, 1800.01, 2350]
+        when 2350.01..3600
+          [2, 2350.01, 3600]
+        when 3600.01..6500
+          [3, 3600.01, 6500]
+        else
+          [4, 6500.01, 11244]
+        end
+    end
 
 
      def special_family?
@@ -119,29 +121,29 @@ module Core
 
 
       def list
-      array = Array.new
+        array = Array.new
 
 
-      list_rii          = rii
-      list_rie          = rie
-      list_olders       = olders
-      list_vulnerables  = vulnerables
-      list_specials     = specials
+        list_rii          = rii
+        list_rie          = rie
+        list_olders       = olders
+        list_vulnerables  = vulnerables
+        list_specials     = specials
 
-      array << list_rii         unless list_rii.nil?
-      array << list_rie         unless list_rie.nil?
+        array << list_rii         unless list_rii.nil?
+        array << list_rie         unless list_rie.nil?
 
-      if self.current_situation_id != 2
-        array << list_olders      unless list_olders.nil?
-        array << list_vulnerables unless list_vulnerables.nil?
-        array << list_specials    unless list_specials.nil?
-      end
+        if self.current_situation_id != 2
+          array << list_olders      unless list_olders.nil?
+          array << list_vulnerables unless list_vulnerables.nil?
+          array << list_specials    unless list_specials.nil?
+        end
 
-      array.each_with_index do |a, i|
-        array[i] << position(array[i])
-      end
+        array.each_with_index do |a, i|
+          array[i] << position(array[i])
+        end
 
-      array
+        array
     end
 
     private
@@ -180,7 +182,7 @@ module Core
             else
 
               sql = "program_id = ? AND
-                     situation_status_id = ?
+                     situation_status_id in (4,54)
                      AND convocation_id > 1524
                      AND procedural_status_id IN(14, 72)
                      AND income BETWEEN ? AND ?"
@@ -188,11 +190,11 @@ module Core
               @geral = Core::View::GeneralPontuation.select(:id)
                                                          .where(sql,
                                                                 self.program_id,
-                                                                self.current_situation_id,
                                                                 array[1][1],
                                                                 array[1][2])
                                                          .map(&:id)
                                                          .find_index(self.id)
+
             end
         else
             case array[0]
@@ -204,12 +206,11 @@ module Core
                        and cadastre_id = general_pontuations.id) > 0)
                        AND convocation_id > 1524
                        AND procedural_status_id IN(14, 72)
-                       AND situation_status_id = ?
+                       AND situation_status_id in (4,54)
                        AND income BETWEEN  ? AND ?"
 
                 @geral = Core::View::GeneralPontuation.select(:id)
                                                            .where(sql,
-                                                                  self.current_situation_id,
                                                                   array[1][1],
                                                                   array[1][2])
                                                            .map(&:id)
@@ -219,14 +220,13 @@ module Core
                         from extranet.candidate_dependents
                         where special_condition_id in (2,3)
                         and cadastre_id = general_pontuations.id) > 0)
-                        and situation_status_id = ?
+                        and situation_status_id in (4,54)
                         and convocation_id > 1524
                         and procedural_status_id IN(14, 72)
                         and income BETWEEN ? AND ?"
 
                 @geral = Core::View::GeneralPontuation.select(:id)
                                                            .where(sql,
-                                                                  self.current_situation_id,
                                                                   array[1][1],
                                                                   array[1][2])
                                                            .map(&:id).find_index(self.id)
