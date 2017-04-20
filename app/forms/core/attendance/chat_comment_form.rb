0f4790a21bb2f83c87e1c140518123d305c579da
igormarjes <igormarjes@gmail.com>
2017-04-20 18:52:55 -0300

    Update chat

--- app/forms/core/attendance/chat_comment_form.rb
+++ app/forms/core/attendance/chat_comment_form.rb
@@ -3,6 +3,7 @@ require_dependency 'core/attendance/chat_comment'
 module Core
   module Attendance
     class ChatCommentForm < Core::Attendance::ChatComment
+      attr_accessor :cadastre_id
       validates :chat_category, presence: true
 
     end
