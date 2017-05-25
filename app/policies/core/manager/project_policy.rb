17922e08127ea83ee41eda7634febfe0b2579745
Elton Silva <elton.chrls@gmail.com>
2017-05-25 13:25:53 -0300

    Adicionando validação de projetos

--- app/policies/core/manager/project_policy.rb
+++ app/policies/core/manager/project_policy.rb
@@ -4,8 +4,10 @@ module Core
   module Manager
     class ProjectPolicy < ApplicationPolicy
       
-      def allow_manager?(user_id)
-        (self.responsible_id == user_id || self.manager_id == user_id)
+      def allow_manager?(user)
+        self.responsible_id == user.id ||
+        self.manager_id == user.id) ||
+        user.administrator?
       end
 
     end
