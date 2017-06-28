12e0f2722ccbcd4a214b38cfcd4e5af78bb2eb1c
Igor <igormarjes@gmail.com>
2017-06-28 14:12:36 -0300

    Update requriment

--- app/forms/core/protocol/requeriment_form.rb
+++ app/forms/core/protocol/requeriment_form.rb
@@ -5,6 +5,7 @@ module Core
     class RequerimentForm < Core::Protocol::Assessment
 
       validates :observation, presence: true
+      validates :cpf, cpf: true
       default_scope { where(prefex: 777, subject_id: [1746,1747]) }
 
       scope :by_type, -> (type) {
@@ -19,16 +20,32 @@ module Core
 
 
       belongs_to :general, class_name: ::Core::View::GeneralPontuation, foreign_key: :cpf, primary_key: :cpf
+      belongs_to :cadastre, class_name: ::Core::Candidate::Cadastre, foreign_key: :cpf, primary_key: :cpf
 
       scope :by_candidate_situation, -> (situation_status_id) {
         joins(:general)
         .where('extranet.general_pontuations.situation_status_id =  ?', situation_status_id)
       }
 
+      scope :by_program, -> (program) {
+        joins(:cadastre)
+        .where('extranet.candidate_cadastres.program_id =  ?', program)
+      }
+
+      scope :without_bond, -> (bond){
+        if bond == "true"
+          joins('inner join extranet.candidate_cadastres  on candidate_cadastres.cpf = extranet.protocol_assessments.cpf ')
+          .where('extranet.candidate_cadastres.program_id in (3,6)')
+        else
+          joins('left join extranet.candidate_cadastres  on candidate_cadastres.cpf = extranet.protocol_assessments.cpf ')
+          .where('extranet.candidate_cadastres.id is null')
+        end
+      }
+
 
       def self.situations
         situations = {}
-        
+
         all.each do |situation|
           if situation.cadastre.present?
             situations[situation.cadastre.presenter.current_situation_name] = situation.cadastre.presenter.current_situation_id rescue nil
