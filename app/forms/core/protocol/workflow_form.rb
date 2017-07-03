e6ea6702db429b14cd18c993cdea6e86713e28e0
Elton Silva <elton.chrls@gmail.com>
2017-07-03 12:08:04 -0300

    Atualização formulário de workflow

--- app/forms/core/protocol/workflow_form.rb
+++ app/forms/core/protocol/workflow_form.rb
@@ -4,7 +4,7 @@ module Core
   module Protocol
     class WorkflowForm < Core::Protocol::Workflow # :nodoc: 
 
-      attr_accessor :attachment_number
+      attr_accessor :attachment_number, :attachment_type
       
       before_validation :set_attachment
 
@@ -14,7 +14,8 @@ module Core
       private
 
       def set_attachment
-        document = Core::Protocol::Assessment.find_by(document_number: self.attachment_number) rescue nil
+        document = Core::Protocol::Assessment.where(document_number: self.attachment_number,
+                                                    document_type: self.attachment_type).first rescue nil
         
         if document.nil?
           errors.add(:attachment_number, "Documento não existe")
