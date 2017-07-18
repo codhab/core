c3c51e60c940bd43cabb55db3730c44d515fb7f3
Igor <igormarjes@gmail.com>
2017-07-18 15:25:19 -0300

    update

--- app/presenters/core/document/data_print_presenter.rb
+++ app/presenters/core/document/data_print_presenter.rb
@@ -11,13 +11,18 @@ module Core
         template = template.gsub('doc.expeditor', self.rg_org).gsub('doc.uf_rg', self.rg_uf)
         template = template.gsub('doc.processo', self.document_number)
         if self.spouse_cpf.present?
-          template = template.gsub('doc.nome_conjuge', " e #{self.spouse_name} ")
-          template = template.gsub('doc.cpf_conjuge', ", inscrito no CPF nº #{self.spouse_cpf} ").gsub('doc.nacionalidade_conjuge', self.spouse_nationality)
-          template = template.gsub('doc.profissao_conjuge', self.spouse_employment).gsub('doc.est_civil_conjuge', self.spouse_civil_state.name) if self.spouse_civil_state.present?
-          template = template.gsub('doc.rg_conjuge', ", e portador da CI nº #{self.spouse_rg} ").gsub('doc.rg_org_conjuge', self.spouse_rg_org)
-          template = template.gsub('doc.rg_uf_conjuge', self.spouse_rg_uf)
+          template = template.gsub('doc.conjuge_nome', " e #{self.spouse_name} ")
+          template = template.gsub('doc.conj_cpf', ", inscrito no CPF nº #{self.spouse_cpf} ").gsub('doc.nac_conjuge', self.spouse_nationality)
+          template = template.gsub('doc.prof_conju', self.spouse_employment).gsub('doc.est_civil', self.spouse_civil_state.name) if self.spouse_civil_state.present?
+          template = template.gsub('doc.registro_geral', ", e portador da CI nº #{self.spouse_rg} ").gsub('doc.reg_exp', self.spouse_rg_org)
+          template = template.gsub('doc.reg_uf', self.spouse_rg_uf)
           template = template.gsub('doc.regime_casamento', self.wedding_regime)
           template = template.gsub('doc.data_casamento', self.wedding_date)
+        else
+          template = template.gsub('doc.conjuge_nome,', "").gsub('doc.conj_cpf,', "").gsub('doc.nac_conjuge,', "")
+          template = template.gsub('doc.prof_conju,', "").gsub('doc.conj_cpf,', "").gsub('doc.est_civil,', "")
+          template = template.gsub('doc.registro_geral,', "").gsub('doc.reg_exp,', "").gsub('doc.reg_uf,', "")
+          template = template.gsub('doc.regime_casamento,', "").gsub('doc.conj_cpf,', "").gsub('doc.data_casamento,', "")
         end
         template = template.gsub('doc.cidade', self.city.name) if self.city.present?
         template = template.gsub('doc.estado', self.city.state.name) if self.city.present?
