0729791e399bf42cecb419c024645c345c569353
Igor <igormarjes@gmail.com>
2017-07-21 15:19:22 -0300

    update entity

--- app/models/core/entity/interest.rb
+++ app/models/core/entity/interest.rb
@@ -5,7 +5,7 @@ module Core
     class Interest < ApplicationRecord
       self.table_name = 'extranet.entity_interests'
 
-      belongs_to :unit_sale, class_name: ::Core::Address::City
+      belongs_to :unit_sale, class_name: ::Core::Entity::UnitSale
       belongs_to :entity,    class_name: ::Core::Entity::Cadastre, foreign_key: :entity_id
 
     end
