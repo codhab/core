d608622bba245548de9303151c42d42968a7947f
Victor Vitalino <vitalino.victor@gmail.com>
2017-08-09 16:09:54 -0300

    schedule model fix

--- app/models/core/social_work/candidate_schedule.rb
+++ app/models/core/social_work/candidate_schedule.rb
@@ -4,6 +4,8 @@ module Core
   module SocialWork
     class CandidateSchedule < ApplicationRecord
       self.table_name = 'generic.social_work_candidate_schedules'
+      belongs_to :schedule_status
+      has_many :schedule_interections
     end
   end
 end
