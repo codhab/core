61a6e999e3a84a33cecc45a2274414cd5f821f31
Victor Vitalino <vitalino.victor@gmail.com>
2017-08-09 11:38:19 -0300

    fix

--- app/models/core/social_work/candidate.rb
+++ app/models/core/social_work/candidate.rb
@@ -4,6 +4,12 @@ module Core
   module SocialWork
     class Candidate < ApplicationRecord
       self.table_name = 'generic.social_work_candidates'
+
+      has_many :dependents
+      has_many :candidate_schedules
+      has_many :candidate_projects
+      belongs_to :city,                   required: false,          class_name: ::Core::Address::City
+      belongs_to :civil_state,              required: false,          class_name: ::Core::Candidate::CivilState
     end
   end
 end
