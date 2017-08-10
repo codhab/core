ac50eaf3b37b107153827afe825dc7ba83db3e78
Victor Vitalino <vitalino.victor@gmail.com>
2017-08-10 16:56:19 -0300

    fix upload

--- app/models/core/social_work/document_upload.rb
+++ app/models/core/social_work/document_upload.rb
@@ -5,6 +5,7 @@ module Core
     class DocumentUpload < ApplicationRecord
       self.table_name = 'generic.social_work_document_uploads'
       belongs_to :document_category
+      mount_uploader :document, Core::SocialWork::DocumentUploader
     end
   end
 end
