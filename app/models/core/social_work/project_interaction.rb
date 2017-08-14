a74fbe4c82474c16b835981b286025e1ad41fe70
Victor Vitalino <vitalino.victor@gmail.com>
2017-08-14 11:21:36 -0300

    fix

--- app/models/core/social_work/project_interaction.rb
+++ app/models/core/social_work/project_interaction.rb
@@ -2,8 +2,8 @@ require_dependency 'core/application_record'
 
 module Core
   module SocialWork
-    class ProjectInterection < ApplicationRecord
-      self.table_name = 'generic.social_work_project_interections'
+    class ProjectInteraction < ApplicationRecord
+      self.table_name = 'generic.social_work_project_interactions'
       belongs_to :candidate_project
     end
   end
