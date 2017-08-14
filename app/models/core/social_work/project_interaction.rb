f6fe645d4eb28f1e347114df8fc032143f3f8ace
Victor Vitalino <vitalino.victor@gmail.com>
2017-08-14 14:40:51 -0300

    fix name

--- app/models/core/social_work/project_interaction.rb
+++ app/models/core/social_work/project_interaction.rb
@@ -5,6 +5,8 @@ module Core
     class ProjectInteraction < ApplicationRecord
       self.table_name = 'generic.social_work_project_interactions'
       belongs_to :candidate_project
+      mount_uploader :document_one, Core::SocialWork::DocumentUploader
+      mount_uploader :document_two, Core::SocialWork::DocumentUploader
     end
   end
 end
