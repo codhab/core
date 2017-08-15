dc13b64744a2e0210e6ceefce7260b5cd1bbef10
Victor Vitalino <vitalino.victor@gmail.com>
2017-08-15 12:06:17 -0300

    fix dependents

--- app/models/core/social_work/dependent.rb
+++ app/models/core/social_work/dependent.rb
@@ -6,6 +6,8 @@ module Core
       self.table_name = 'generic.social_work_dependents'
       belongs_to :candidate
       belongs_to :civil_state,              required: false,          class_name: ::Core::Candidate::CivilState
+      belongs_to :kinship,                 required: false,  class_name: ::Core::Candidate::Kinship
+      belongs_to :gender,                 required: false,  class_name: ::Core::Candidate::Gender
     end
   end
 end
