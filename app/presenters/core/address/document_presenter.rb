c7238a0f6e21daefc0b3297dea0a52bca94e5357
Igor <igormarjes@gmail.com>
2017-08-25 11:20:35 -0300

    update document

--- app/presenters/core/address/document_presenter.rb
+++ app/presenters/core/address/document_presenter.rb
@@ -10,13 +10,13 @@ module Core
           @text = "Observação: Beneficiário originário - O Distrito Federal, por seu representante legal, DECLARA Suspensa
                    a Exigibilidade do Imposto sobre a Transmissão Causa Mortis ou Doação de Quaisquer Bens e Direitos  -
                    ITCD, fundamentada na Lei 4.997/2012 de 19 de dezembro de 2012, Conforme   ato   declaratório  n º #{unit.notary_office.declaratory_act_number}
-                   - GEESP/COTRI/SUREC/SEEF, de #{unit.notary_office.date_act_declaratory.strftime('%d/%m/%Y')}"
+                   - GEESP/COTRI/SUREC/SEEF, de #{unit.notary_office.date_act_declaratory.strftime('%d/%m/%Y') rescue nil}"
                    @description = "Tipo de Ficha - Suspenso ITBI"
         when 2
           @text = "Observação: Beneficiário originário, isento de Imposto sobre Transmissão causa  mortis  e  doação  de
                   quaisquer bens ou direitos  - ITCD, na forma da Lei Complementar nº 353/2001,   regulamentada  pelo
                   Decreto nº 21.972/2001, conforme Ato Declaratório DITRI/SUREC/SEF #{unit.notary_office.declaratory_act_number} publicado
-                  #{unit.notary_office.date_act_declaratory.strftime('%d/%m/%Y')}."
+                  #{unit.notary_office.date_act_declaratory.strftime('%d/%m/%Y') rescue nil}."
                   @description = "Tipo de Ficha - Isento"
         when 3
           @text = "Observação: Beneficiário não originário,  sem isenção de Imposto sobre Transmissão Causa Mortis e Doação de Quaisquer Bens ou Direito
