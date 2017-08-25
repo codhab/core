12d728d2492a535d1222c1a2e2a77f8a116c4eeb
Elton Silva <elton.chrls@gmail.com>
2017-08-25 11:14:39 -0300

    Correção de busca por nome

--- app/models/core/view/general_pontuation.rb
+++ app/models/core/view/general_pontuation.rb
@@ -12,12 +12,17 @@ module Core
       scope :by_income, -> (started_at = 0, ended_at = 1600) { where(income: started_at..ended_at) }
       scope :by_cpf, -> (cpf) {
 
-        current = find_by(cpf: cpf.to_s.unformat_cpf)
+        current = find_by(cpf: cpf.to_s.unformat_cpf) rescue nil
 
-        where(total: (current.total - 0.400)..(current.total + 0.400))
+        if current.nil?
+          all
+        else
+          where(total: (current.total - 0.400)..(current.total + 0.400))
+        end
       }
       scope :by_name, -> (name) {
 
+        name = name.strip!
         where("name ILIKE '%#{name}%' ")
 
       }
