42f04f0981e0fd4540f3e07032e2713d6e2a65e0
canamari <cleubercanamari11@gmail.com>
2018-01-24 11:38:40 -0200

    atualisacao social work cadastre

--- app/models/core/social_work_cadastre/location_select.rb
+++ app/models/core/social_work_cadastre/location_select.rb
@@ -2,7 +2,7 @@ module Core
   module SocialWorkCadastre
     class LocationSelect < ApplicationRecord
       self.table_name = 'portal.social_work_cadastre_location_selects'
-      belongs_to :city, class_name: '::Address::City', foreign_key: 'city_id'
+      belongs_to :city, class_name: ::Core::Address::City, foreign_key: 'city_id'
     end
   end
 end
