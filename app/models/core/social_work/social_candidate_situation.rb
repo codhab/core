04bb0fc9de6515f86188d1c07487b4886b54fd4c
Igor Marques <igormarjes@gmail.com>
2018-02-16 17:37:02 -0200

    Atualização de melhorias habitacionais

--- app/models/core/social_work/social_candidate_situation.rb
+++ app/models/core/social_work/social_candidate_situation.rb
@@ -8,10 +8,12 @@ module Core
       belongs_to :candidate, class_name: 'Core::SocialWork::SocialCandidate'
       belongs_to :situation, class_name: 'Core::SocialWork::SocialSituationType'
 
-      scope :delivered, -> {
+      scope :delivered, ->{
         where(id: Core::SocialWork::SocialCandidateSituation
                   .select('max(id)').group(:candidate_id), situation_id: 3)
       }
+
+      scope :countable, ->{ joins(:situation).where('social_situation_types.countable is true') }
 
     end
   end
