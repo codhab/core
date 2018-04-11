07a2f8ba21277938a6cd082fd61378c6af50fb8f
Victor Vitalino <vitalino.victor@gmail.com>
2018-04-11 17:19:57 -0300

    fix

--- app/models/core/regularization/solicitation.rb
+++ app/models/core/regularization/solicitation.rb
@@ -5,6 +5,10 @@ module Core
   module Regularization
     class Solicitation < ApplicationRecord
       self.table_name = 'extranet.regularization_solicitations'
+      belongs_to :unit,          required: false, class_name: 'Core::Address::Unit', foreign_key: :unit_id
+      belongs_to :city,          required: false, class_name: 'Core::Address::City'
+
+      scope :by_city,     ->(city)    { where(city_id: city) }
     end
   end
 end
