51e558a0bfb7bea02478a9cd0e93dde92b8d6824
Cleuber <cleubercanamari11@gmail.com>
2018-05-03 17:02:52 -0300

    att cadastres

--- app/models/core/candidate/cadastre_situation.rb
+++ app/models/core/candidate/cadastre_situation.rb
@@ -8,6 +8,7 @@ module Core
       belongs_to :situation_status,      required: false, class_name: ::Core::Candidate::SituationStatus
       belongs_to :cadastre_convocation,  required: false, class_name: ::Core::Candidate::CadastreConvocation
       belongs_to :cadastre,              required: false, class_name: ::Core::Candidate::Cadastre
+      belongs_to :staff,                 required: false, class_name: ::Core::Person::Staff, foreign_key: 'staff_id'
     end
   end
 end
