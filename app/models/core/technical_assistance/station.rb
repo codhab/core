3a4184caa1af3b9ef8c1b9f58d7d940ea62098c0
Cleuber <cleubercanamari11@gmail.com>
2018-06-13 10:48:43 -0300

    att tec ass

--- app/models/core/technical_assistance/station.rb
+++ app/models/core/technical_assistance/station.rb
@@ -2,6 +2,7 @@ module Core
   module TechnicalAssistance
     class Station < ApplicationRecord
       self.table_name = 'extranet.technical_assistance_stations'
+      belongs_to :city, required: false, class_name: ::Core::Address::City
     end
   end
 end
